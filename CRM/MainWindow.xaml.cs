4f31c2d6dce16a94bef453e259e6e10fcf1f1c7d
Miguel <miguelisto09@gmail.com>
2025-01-10 17:26:58 -0400

    Pantalla Principal

--- CRM/MainWindow.xaml.cs
+++ CRM/MainWindow.xaml.cs
@@ -1,24 +1,26 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace CRM
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            // Animación de difuminado
+            var fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.5));
+            fadeOut.Completed += (s, a) => this.Close();
+            this.BeginAnimation(Window.OpacityProperty, fadeOut);
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
